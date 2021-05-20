0f899d5468332fc8668e46ad08a1cef1cb78a9aa
Sebi <SebastianStoff@gmx.at>
2021-05-20 18:21:28 +0200

    #4. handling fluid styling via layout component

--- src/App/components/Layout/index.tsx
+++ src/App/components/Layout/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Navbar } from "./Navbar"
 
 interface Props {
+  fluid?: boolean;
   children?: any
 }
 
@@ -11,8 +12,8 @@ export const Layout: React.FC<Props> = (props) => {
   return <>
     <div className="text-gray-800">
       <Navbar></Navbar>
-      <div className="m-3">
-        {props.children}
+      <div className={`m-3 ${ props.fluid ? '' : 'container xl:mx-auto xl:px-40'}`}>
+          {props.children}
       </div>
     </div>
     
--- src/App/pages/About/index.tsx
+++ src/App/pages/About/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { Layout } from "../../components/Layout";
+import { FormattedMessage } from "react-intl";
+import { Heading } from "../../components/Heading";
 
 export const About = () => {
 
-  return <Layout>About!</Layout>
+  return (
+    <Layout>
+        <Heading>
+          <FormattedMessage
+            defaultMessage="About"
+            description="About page"
+          ></FormattedMessage>
+        </Heading>
+    </Layout>
+    
+  )
 
 }
 
