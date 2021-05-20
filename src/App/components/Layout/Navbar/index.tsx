809218afe339b2a939edb4f041db8d64ae27b48b
Sebi <SebastianStoff@gmx.at>
2021-05-20 12:38:46 +0200

    #4. calling constant instead of hardcoding

--- src/App/components/Layout/Navbar/index.tsx
+++ src/App/components/Layout/Navbar/index.tsx
@@ -2,14 +2,15 @@ import { FormattedMessage } from "react-intl";
 import { Link } from "react-router-dom";
 import { NampiLogo } from "../../NampiLogo";
 import { NampiLangSelect } from "../../NampiLangSelect";
+import { APP_ROUTES } from "../../../constants";
 
 export const Navbar = () => (
   <nav className="relative flex items-center justify-between flex-wrap bg-gray-400 p-3 text-white">
     <div className="space-x-3 text-white flex items-center">
-      <Link to="/" className="font-semibold text-2xl">
+      <Link to={APP_ROUTES.HOME} className="font-semibold text-2xl">
         <NampiLogo className="h-10 bg-white p-1 rounded" />
       </Link>
-      <Link to="/persons">
+      <Link to={APP_ROUTES.PERSONS}>
         <FormattedMessage
           description="The persons link label"
           defaultMessage="Persons"
@@ -33,7 +34,7 @@ export const Navbar = () => (
           defaultMessage="Sources"
         />
       </Link>
-      <Link to="/about">
+      <Link to={APP_ROUTES.ABOUT}>
         <FormattedMessage
           description="About page"
           defaultMessage="About"
