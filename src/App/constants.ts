43968b45068b4f19d8ed289ec53752b34a44fed4
Sebi <SebastianStoff@gmx.at>
2021-05-20 12:37:51 +0200

    #4. handling base routes now via constant

--- src/App/constants.ts
+++ src/App/constants.ts
@@ -1,2 +1,13 @@
 import { createBrowserHistory } from "history";
 export const HISTORY = createBrowserHistory();
+
+// path prefixing will be handled by React Router's baseName prop if necessary
+export const APP_ROUTES = {
+  HOME:"/",
+  ABOUT: "/about",
+  TEAM: "/team",
+  IMPRINT: "/imprint",
+  PERSONS:"/persons",
+  SEARCH:"/search",
+  SOURCES: "/sources"
+}
