f68d53a4648159aad7384887ae9e21d76f536d8e
Sebi <SebastianStoff@gmx.at>
2021-05-25 14:00:07 +0200

    #12. using lang constants now to display lang selection

--- src/App/components/NampiLangSelect/index.tsx
+++ src/App/components/NampiLangSelect/index.tsx
@@ -1,24 +1,41 @@
 import React from "react";
-import {Select} from "../Select";
+import { Select } from "../Select";
 import { HTMLProps } from "react";
 import { useNampiLang } from "App/hooks/useNampiLang";
+import { Locale } from "../../../I18n/Locale";
+import { LANGUAGE_NAMES } from "../../../I18n/constants";
 
 interface Props extends HTMLProps<HTMLSelectElement> {}
 
 export const NampiLangSelect: React.FC<Props> = ({ className, ...props }) => {
+  const { lang, changeLang } = useNampiLang();
 
-  const { lang, changeLang } =  useNampiLang();
-
-  const handleLangChange = (evt: React.SyntheticEvent<HTMLSelectElement, Event>) => {
-    console.log("CHANGE LANG");
+  const handleLangChange = (
+    evt: React.SyntheticEvent<HTMLSelectElement, Event>
+  ) => {
     changeLang(evt.currentTarget.value as any);
-  }
+  };
+
+  /**
+   * Renders options according to NAMPIs Locale constant type.
+   * @returns
+   */
+  const renderOptions = () => {
+    let keys = Object.keys(Locale);
+    return keys.map((key) => (
+      <option value={(Locale as { [property: string]: string })[key]} selected={(Locale as { [property: string]: string })[key] === lang}>
+        {
+          (LANGUAGE_NAMES as { [property: string]: string })[
+            (Locale as { [property: string]: string })[key]
+          ]
+        }
+      </option>
+    ));
+  };
 
   return (
     <Select className="text-black" {...props} onChange={handleLangChange}>
-      <option>{lang}</option>
-      <option>de</option>
-      <option>en</option>
+      {renderOptions()}
     </Select>
-  )
-}
+  );
+};
