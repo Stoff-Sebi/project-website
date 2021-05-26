b1042ec3580356fea223a2325d7a95d311bb24c0
Sebi <SebastianStoff@gmx.at>
2021-05-26 08:43:38 +0200

    #2. Added some content from imprint

--- src/App/pages/Imprint/index.tsx
+++ src/App/pages/Imprint/index.tsx
@@ -1,12 +1,33 @@
-
+import { Heading } from "App/components/Heading";
 import { Layout } from "App/components/Layout";
 import React from "react";
-
+import { FormattedMessage } from "react-intl";
 
 export const Imprint: React.FC = () => {
-  
-  return <Layout><p>Imprint</p></Layout>
-
-}
+  return (
+    <Layout>
+      <Heading
+        className="text-5xl">
+        <FormattedMessage
+          defaultMessage="Imprint"
+          description="Main heading for imprint page"
+        ></FormattedMessage>
+      </Heading>
+      <br></br>
+      <Heading>
+        <FormattedMessage
+          defaultMessage="Publisher, development and implementation"
+          description="Heading for development section."
+        ></FormattedMessage>
+      </Heading>
+      <p>Developed by the NAMPI Project. Project partners are:</p>
+      <ul>
+        <li>University of Vienna, Institute for History</li>
+        <li>International Centre for Archival Research</li>
+        <li>ZIM Institute Centre for Information Modelling, University of Graz</li>
+      </ul>
+    </Layout>
+  );
+};
 
 export default Imprint;
