193b15ed14d6d4d5b3c6f32fb0eb7009bde19415
Sebi <SebastianStoff@gmx.at>
2021-05-26 17:46:27 +0200

    Added active style to navlinks

--- src/App/components/Layout/Navbar/index.tsx
+++ src/App/components/Layout/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import { FormattedMessage } from "react-intl";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { NampiLogo } from "../../NampiLogo";
 import { NampiLangSelect } from "../../NampiLangSelect";
 import { APP_ROUTE } from "../../../constants";
@@ -12,39 +12,39 @@ export const Navbar: React.FC<Props> = (props) => (
 
   <nav className="sticky top-0 relative flex items-center justify-between flex-wrap bg-gray-400 p-3 text-white text-base">
     <div className="space-x-3 text-white flex items-center">
-      <Link to={`/${props.lang}${APP_ROUTE.HOME}`} className="font-semibold text-2xl">
+      <NavLink to={`/${props.lang}${APP_ROUTE.HOME}`} className="font-semibold text-2xl">
         <NampiLogo className="h-10 bg-white p-1 rounded" />
-      </Link>
-      <Link to={`/${props.lang}${APP_ROUTE.PERSONS}`}>
+      </NavLink>
+      <NavLink activeClassName="text-black" to={`/${props.lang}${APP_ROUTE.PERSONS}`}>
         <FormattedMessage
           description="The persons link label"
           defaultMessage="Persons"
         />
-      </Link>
-      <Link to="/persons">
+      </NavLink>
+      <NavLink to="/persons" activeClassName="text-black">
         <FormattedMessage
           description="The persons link label"
           defaultMessage="Events"
         />
-      </Link>
-      <Link to="/persons">
+      </NavLink>
+      <NavLink to="/persons" activeClassName="text-black">
         <FormattedMessage
           description="The persons link label"
           defaultMessage="Search"
         />
-      </Link>
-      <Link to="/persons">
+      </NavLink>
+      <NavLink to="/persons" activeClassName="text-black">
         <FormattedMessage
           description="The persons link label"
           defaultMessage="Sources"
         />
-      </Link>
-      <Link to={`/${props.lang}${APP_ROUTE.ABOUT}`}>
+      </NavLink>
+      <NavLink to={`/${props.lang}${APP_ROUTE.ABOUT}`} activeClassName="text-black">
         <FormattedMessage
           description="About page"
           defaultMessage="About"
         />
-      </Link>
+      </NavLink>
     </div>
     <div className="">
         <NampiLangSelect className="text-black"/>
